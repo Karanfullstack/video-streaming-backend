3d87eb9559b8472847b2a11dc87dc36f67540675
Karan Chauhan <karanchouhan211@gmail.com>
2024-12-07 20:26:54 +0100

    add user model

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const CONFIG = {
+    ACCESS_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_SECRET: process.env.REFRESH_TOKEN,
+    EXPIRY_TIME: process.env.EXPIRY_TIME_TOKEN,
+};
