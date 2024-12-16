de73bd61914150511a85857d2087e3a2dd01681d
Kasim Kazmi <kasimkazmi27@gmail.com>
2024-12-16 17:38:24 -0500

    add user authentication and session management features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,113 +4,145 @@ import { asynHandler } from "../utils/asyncHandler.js";
 import { uploadToCloudinary } from "../utils/cloudinary.js";
 import fs from "fs";
 import { generateAccessAndRefreshToken } from "../utils/generateTokens.js";
+import { cookiesOptions } from "../const/index.js";
 
 // @Create User End Point
 const registerUser = asynHandler(async (req, res) => {
-    const { username, email, firstName, lastName, password } = req.body;
-    if ([username, email, firstName, lastName, password].some((value) => value.trim() === "")) {
-        throw new HttpError(400, "All fields are required!");
+  const { username, email, firstName, lastName, password } = req.body;
+  if (
+    [username, email, firstName, lastName, password].some(
+      (value) => value.trim() === ""
+    )
+  ) {
+    throw new HttpError(400, "All fields are required!");
+  }
+
+  const isUserExists = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserExists) {
+    if (fs.existsSync(req.files?.avatar[0]?.path)) {
+      fs.unlinkSync(req.files?.avatar[0]?.path);
     }
-
-    const isUserExists = await User.findOne({
-        $or: [{ username }, { email }],
-    });
-
-    if (isUserExists) {
-        if (fs.existsSync(req.files?.avatar[0]?.path)) {
-            fs.unlinkSync(req.files?.avatar[0]?.path);
-        }
-        if (fs.existsSync(req.files?.coverPath[0]?.path)) {
-            fs.unlinkSync(req.files?.coverPath[0]?.path);
-        }
-        throw new HttpError(403, " user is already exists");
+    if (fs.existsSync(req.files?.coverPath[0]?.path)) {
+      fs.unlinkSync(req.files?.coverPath[0]?.path);
     }
+    throw new HttpError(403, " user is already exists");
+  }
 
-    let localCoverPath;
-    if (req.files && Array.isArray(req.files.coverPath) && req.files.coverPath.length > 0) {
-        localCoverPath = req.files.coverPath[0].path;
-    }
-    const localAvatarPath = req.files?.avatar[0]?.path;
+  let localCoverPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverPath) &&
+    req.files.coverPath.length > 0
+  ) {
+    localCoverPath = req.files.coverPath[0].path;
+  }
+  const localAvatarPath = req.files?.avatar[0]?.path;
 
-    if (!localAvatarPath) throw new HttpError(400, "Avatar is required");
+  if (!localAvatarPath) throw new HttpError(400, "Avatar is required");
 
-    // Upolad to cloudinary
+  // Upolad to cloudinary
 
-    const avatar = await uploadToCloudinary(localAvatarPath).catch((error) => console.log(error));
-    if (!avatar) throw new HttpError(400, "Avatar file is required!!");
+  const avatar = await uploadToCloudinary(localAvatarPath).catch((error) =>
+    console.log(error)
+  );
+  if (!avatar) throw new HttpError(400, "Avatar file is required!!");
 
-    const coverImage = await uploadToCloudinary(localCoverPath).catch((error) =>
-        console.log(error),
-    );
+  const coverImage = await uploadToCloudinary(localCoverPath).catch((error) =>
+    console.log(error)
+  );
 
-    // Save user data into database
-    const user = await User.create({
-        firstName,
-        lastName,
-        avatar: {
-            public_id: avatar.public_id,
-            url: avatar.secure_url,
-        },
-        coverImage: {
-            public_id: coverImage?.public_id || "",
-            url: coverImage?.secure_url || "",
-        },
-        email,
-        username: username.toLowerCase(),
-        password,
-    });
+  // Save user data into database
+  const user = await User.create({
+    firstName,
+    lastName,
+    avatar: {
+      public_id: avatar.public_id,
+      url: avatar.secure_url,
+    },
+    coverImage: {
+      public_id: coverImage?.public_id || "",
+      url: coverImage?.secure_url || "",
+    },
+    email,
+    username: username.toLowerCase(),
+    password,
+  });
 
-    const createdUser = await User.findById(user._id).select("-password -refreshToken -__v");
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -__v"
+  );
 
-    if (!createdUser) throw new HttpError(500, "User registration failed");
+  if (!createdUser) throw new HttpError(500, "User registration failed");
 
-    return res.status(201).json({
-        success: true,
-        data: createdUser,
-        message: "user registred successfully",
-    });
+  return res.status(201).json({
+    success: true,
+    data: createdUser,
+    message: "user registred successfully",
+  });
 });
 
 // @Login User End Point
 const loginUser = asynHandler(async (req, res) => {
-    const { email, username, password } = req.body;
+  const { email, username, password } = req.body;
 
-    if (!(email || username)) {
-        throw new HttpError(401, "User or email is required");
-    }
-    const user = await User.findOne({
-        $or: [{ email }, { username }],
+  if (!(email || username)) {
+    throw new HttpError(401, "User or email is required");
+  }
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new HttpError(404, "Email or Usrname is incorrect.");
+  }
+
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    throw new HttpError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+  const loggedInuser = await User.findById(user._id).select(
+    " -password -refreshToken"
+  );
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, cookiesOptions)
+    .cookie("refreshToken", refreshToken, cookiesOptions)
+    .json({
+      success: true,
+      data: loggedInuser,
+      accessToken,
+      refreshToken,
     });
-
-    if (!user) {
-        throw new HttpError(404, "Email or Usrname is incorrect.");
-    }
-
-    const isMatch = await user.comparePassword(password);
-
-    if (!isMatch) {
-        throw new HttpError(401, "Invalid user credentials");
-    }
-
-    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
-    const loggedInuser = await User.findById(user._id).select(" -password -refreshToken");
-
-    const options = {
-        httpOnly: true,
-        secure: true,
-        sameSite: "None",
-    };
-
-    return res
-        .status(200)
-        .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", refreshToken, options)
-        .json({
-            success: true,
-            data: loggedInuser,
-            accessToken,
-            refreshToken,
-        });
 });
 
-export { loginUser, registerUser };
+//@Get Current User End Point
+const self = asynHandler(async (req, res) => {
+  const user = req.user;
+  return res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+//@Logout User
+const logoutUser = asynHandler(async (req, res) => {
+  await User.findByIdAndUpdate(req.user._id, {
+    $unset: { refreshToken: 1 },
+  });
+  return res
+    .status(200)
+    .clearCookie("accessToken", cookiesOptions)
+    .clearCookie("refreshToken", cookiesOptions)
+    .json({ success: true, message: "User has been logged out Successfully" });
+});
+
+export { loginUser, registerUser, self, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,35 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, self } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/upload.middleware.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
 
 // @Register Route
 router.post(
-    "/register",
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1,
-        },
-        {
-            name: "coverPath",
-            maxCount: 1,
-        },
-    ]),
-    registerUser,
+  "/register",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverPath",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
 );
 
 // @Login Route
 router.get("/login", loginUser);
+
+// @Self Route
+router.get("/self", authenticate,self);
+
+
+// @Logout  Route
+router.post("/logout", authenticate,logoutUser);
+
 
 export default router;
